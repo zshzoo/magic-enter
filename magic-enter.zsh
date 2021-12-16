d67679bb7a74519d426a025db12e05bd95ec8a30
mattmc3 <mattmc3@gmail.com>
2021-12-16 15:54:59 -0500

    Update magic enter command

--- magic-enter.zsh
+++ magic-enter.zsh
@@ -3,9 +3,9 @@ if ! typeset -f magic-enter-cmd > /dev/null; then
     function magic-enter-cmd {
         local cmd
         if [[ "$OSTYPE" == darwin* ]]; then
-            cmd="ls -FG"
+            cmd="ls -G"
         else
-            cmd="ls"
+            cmd="ls --color=auto"
         fi
         if command git rev-parse --is-inside-work-tree &>/dev/null; then
             cmd="$cmd && git status -sb"
