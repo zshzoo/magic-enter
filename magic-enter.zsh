3e4d08542ec10791e34167847ff0fca7c39f47ba
mattmc3 <mattmc3@gmail.com>
2023-04-10 20:01:47 -0400

    Support zstyle config

--- magic-enter.zsh
+++ magic-enter.zsh
@@ -1,17 +1,20 @@
+#
+# magic-enter - Make your enter key magical by binding a Zsh command to it
+#
 # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/magic-enter
-if ! typeset -f magic-enter-cmd > /dev/null; then
-    function magic-enter-cmd {
-        local cmd
-        if [[ "$OSTYPE" == darwin* ]]; then
-            cmd="ls -G"
-        else
-            cmd="ls --color=auto"
-        fi
-        if command git rev-parse --is-inside-work-tree &>/dev/null; then
-            cmd="$cmd && git status -sb"
-        fi
-        echo $cmd
-    }
+
+if ! (( $+functions[magic-enter-cmd] )); then
+  function magic-enter-cmd {
+    local cmd
+    zstyle -s ':zshzoo:magic-enter' command 'cmd' ||
+      cmd="ls ."
+
+    if command git rev-parse --is-inside-work-tree &>/dev/null; then
+      zstyle -s ':zshzoo:magic-enter' git-command 'cmd' ||
+        cmd="git status -sb ."
+    fi
+    echo $cmd
+  }
 fi
 
 magic-enter() {
@@ -29,17 +32,17 @@ magic-enter() {
 (( ! ${+functions[_magic-enter_accept-line]} )) || return 0
 
 case "$widgets[accept-line]" in
-    # Override the current accept-line widget, calling the old one
-    user:*) zle -N _magic-enter_orig_accept-line "${widgets[accept-line]#user:}"
-        function _magic-enter_accept-line() {
-            magic-enter
-            zle _magic-enter_orig_accept-line -- "$@"
-        } ;;
-    # If no user widget defined, call the original accept-line widget
-    builtin) function _magic-enter_accept-line() {
-            magic-enter
-            zle .accept-line
-        } ;;
+  # Override the current accept-line widget, calling the old one
+  user:*) zle -N _magic-enter_orig_accept-line "${widgets[accept-line]#user:}"
+    function _magic-enter_accept-line() {
+      magic-enter
+      zle _magic-enter_orig_accept-line -- "$@"
+    } ;;
+  # If no user widget defined, call the original accept-line widget
+  builtin) function _magic-enter_accept-line() {
+      magic-enter
+      zle .accept-line
+    } ;;
 esac
 
 zle -N accept-line _magic-enter_accept-line
